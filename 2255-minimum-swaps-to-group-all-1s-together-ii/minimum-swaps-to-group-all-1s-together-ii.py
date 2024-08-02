e9d8a8662e3be5c3c9cc442ac7b0921a3c17b103
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-08-02 18:53:46 +0530

    Time: 649 ms (93.47%) | Memory: 20.4 MB (60.59%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,13 +1,28 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        n = len(nums)
-        windows_size = sum(nums)
-        result = current_zero = windows_size - sum(nums[:windows_size])
-        for i in range(n):
-            if nums[(i + windows_size) %  n] == 0:
-                current_zero += 1
+        cnt = sum(nums)
+        nums += nums # To eliminate the circular property of the array, we can append the original array to itself. Then, we checcnt each subarray of length total.
+        ans = zeroes = nums[:cnt].count(0)
+        for i in range(cnt, len(nums)):
             if nums[i] == 0:
-                current_zero -= 1
-            result = min(result, current_zero)
-        return result
-                     
+                zeroes += 1
+            if nums[i-cnt] == 0:
+                zeroes -= 1
+            ans = min(ans, zeroes) # The number of swaps required is the number of 0’s in the subarray of length cnt
+        return ans
+
+    # or
+
+    def minSwaps(self, nums: List[int]) -> int:
+        cnt = sum(nums)
+        if cnt == 0 or cnt == len(nums):
+            return 0
+
+        nums += nums
+        curOnes = sum(nums[:cnt])
+        ones = curOnes
+        for i in range(len(nums)//2):
+            curOnes = curOnes - nums[i] + nums[i + cnt]
+            ones = max(curOnes, ones)
+        
+        return cnt - ones 
