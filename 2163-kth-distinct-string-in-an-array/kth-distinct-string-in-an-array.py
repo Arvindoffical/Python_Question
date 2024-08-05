11baf4bc20a586d1f352f56483449bea1c9212c8
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-08-05 21:29:28 +0530

    Time: 71 ms (55.16%) | Memory: 16.8 MB (75.33%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        maps = Counter(arr)
+
+        for key, val in maps.items():
+            if val == 1:
+                k -= 1
+            if k == 0:
+                return key
+        return ""   
