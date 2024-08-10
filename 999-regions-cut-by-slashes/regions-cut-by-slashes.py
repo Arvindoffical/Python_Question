8884120b0c38de00bce985e4975d96c85f487730
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-08-10 19:37:44 +0530

    Time: 43 ms (98.42%) | Memory: 16.6 MB (92.49%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.py
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.py
@@ -1,50 +1,60 @@
 class Solution:
     def regionsBySlashes(self, grid: List[str]) -> int:
-        def build_matrix(grid):
-            n, matrix = len(grid), []
-            for i in range(n):
-                row1 = []
-                row2 = []
-                for j in range(n):
-                    if grid[i][j] == "/":
-                        for v in [0, 1]: row1.append(v)
-                        for v in [1, 0]: row2.append(v)
-                    elif grid[i][j] == "\\":
-                        for v in [2, 0]: row1.append(v)
-                        for v in [0, 2]: row2.append(v)
-                    else:
-                        for v in [0, 0]: row2.append(v)
-                        for v in [0, 0]: row1.append(v)
-                matrix.append(row1)
-                matrix.append(row2)
-            return matrix
+        grid_size = len(grid)
+        points_per_side = grid_size + 1
+        total_points = points_per_side * points_per_side
 
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= len(A) or j >= len(A) or A[i][j] != 0: return
+        # Initialize disjoint set data structure
+        parent_array = [-1] * total_points
 
-            A[i][j] = -1
+        # Connect border points
+        for i in range(points_per_side):
+            for j in range(points_per_side):
+                if (
+                    i == 0
+                    or j == 0
+                    or i == points_per_side - 1
+                    or j == points_per_side - 1
+                ):
+                    point = i * points_per_side + j
+                    parent_array[point] = 0
 
-            # horizontal and vertical dfs
-            dfs(i - 1, j)
-            dfs(i, j - 1)
-            dfs(i + 1, j)
-            dfs(i, j + 1)
+        # Set the parent of the top-left corner to itself
+        parent_array[0] = -1
+        region_count = 1  # Start with one region (the border)
 
-            # diagonal dfs
-            if i > 0 and j > 0 and (A[i - 1][j] != A[i][j - 1] or A[i - 1][j] != 1): 
-                dfs(i - 1, j - 1)
-            if j > 0 and i < len(A) - 1 and (A[i][j - 1] != A[i + 1][j] or A[i][j - 1] != 2): 
-                dfs(i + 1, j - 1)
-            if i < len(A) - 1 and j < len(A) - 1 and (A[i][j + 1] != A[i + 1][j] or A[i][j + 1] != 1): 
-                dfs(i + 1, j + 1)
-            if j < len(A) - 1 and i > 0 and (A[i][j + 1] != A[i - 1][j] or A[i][j + 1] != 2): 
-                dfs(i - 1, j + 1)
+        # Process each cell in the grid
+        for i in range(grid_size):
+            for j in range(grid_size):
+                # Treat each slash as an edge connecting two points
+                if grid[i][j] == "/":
+                    top_right = i * points_per_side + (j + 1)
+                    bottom_left = (i + 1) * points_per_side + j
+                    region_count += self._union(
+                        parent_array, top_right, bottom_left
+                    )
+                elif grid[i][j] == "\\":
+                    top_left = i * points_per_side + j
+                    bottom_right = (i + 1) * points_per_side + (j + 1)
+                    region_count += self._union(
+                        parent_array, top_left, bottom_right
+                    )
 
-        res = 0
-        A   = build_matrix(grid)
-        for i in range(len(A)):
-            for j in range(len(A)):
-                if A[i][j] == 0:
-                    res += 1
-                    dfs(i, j)
-        return res
+        return region_count
+
+    def _find(self, parent_array: List[int], node: int) -> int:
+        if parent_array[node] == -1:
+            return node
+
+        parent_array[node] = self._find(parent_array, parent_array[node])
+        return parent_array[node]
+
+    def _union(self, parent_array: List[int], node1: int, node2: int) -> int:
+        parent1 = self._find(parent_array, node1)
+        parent2 = self._find(parent_array, node2)
+
+        if parent1 == parent2:
+            return 1  # Nodes are already in the same set, new region formed
+
+        parent_array[parent2] = parent1  # Union the sets
+        return 0  # No new region formed
