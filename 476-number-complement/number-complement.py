656e87c0a314cbdbc75da9785c3215e117d73a7a
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-08-22 21:20:26 +0530

    Time: 38 ms (30.88%) | Memory: 16.4 MB (89.59%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findComplement(self, num):
+        
+        compliment = 1
+        while compliment <= num:
+            compliment = compliment << 1
+        
+        leftCompliment=compliment - 1
+        compliment=(leftCompliment) ^ num
+        
+        return compliment
+        
+        
