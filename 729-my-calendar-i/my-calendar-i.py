55adf3da59e3a2e8b3ff3a3287fa9790a1913479
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-09-26 23:14:02 +0530

    Time: 313 ms (37.53%) | Memory: 17.4 MB (65.25%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.py
@@ -0,0 +1,20 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.bookings = []  # List to store booked intervals
+
+    def book(self, start: int, end: int) -> bool:
+        # Loop through existing bookings to check for overlap
+        for existingStart, existingEnd in self.bookings:
+            # Check if new event overlaps with an existing one
+            if start < existingEnd and end > existingStart:
+                return False
+        
+        # No overlap, add the event to the list of bookings
+        self.bookings.append((start, end))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
