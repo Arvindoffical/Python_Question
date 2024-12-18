8be5f7d607b239d5cdf1b56f9ddcb8c5d0363f80
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-12-18 22:23:03 +0530

    Time: 4 ms (21.42%) | Memory: 17.7 MB (10.22%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalPrices(self, prices):
+        stack = []
+        for i, price in enumerate(prices):
+            while stack and prices[stack[-1]] >= price:
+                index = stack.pop()
+                prices[index] -= price
+            stack.append(i)
+        return prices
+        
